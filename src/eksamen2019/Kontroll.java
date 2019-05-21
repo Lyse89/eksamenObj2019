826563088a6da506bca20b063ab9f22ad421e97e
Simen Lyse <simenlyse@gmail.com>
2019-05-21 21:07:11 +0200

    la til tilKoblin i Kontroll

--- src/eksamen2019/Kontroll.java
+++ src/eksamen2019/Kontroll.java
@@ -1,5 +1,23 @@
 package eksamen2019;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Kontroll {
+	
+	private String databasenavn = "jdbc:mysql://localhost:3306/kurs";
+    private String databasedriver = "com.mysql.jdbc.Driver";
+    private Connection forbindelse;
+    private ResultSet resultat;
+    private Statement utsagn;
+	
+    public void kobleTil() throws Exception {        
+    	try {
+                forbindelse = DriverManager.getConnection(databasenavn,"root","OBJ2100");
+    		} catch(Exception e) {                    
+                throw new Exception("Kan ikke oppnå kontakt med databasen");                        
+    		}            
+        }
 
 }
