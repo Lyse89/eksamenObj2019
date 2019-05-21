7e781336ebf1005699e4167c2588816c0535ad9d
Simen Lyse <simenlyse@gmail.com>
2019-05-21 10:32:47 +0200

    starten

---
+++ src/eksamen2019/DialogStudent.java
@@ -0,0 +1,14 @@
+package eksamen2019;
+
+public class DialogStudent {
+
+	public DialogStudent(Main main) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setVisible(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
