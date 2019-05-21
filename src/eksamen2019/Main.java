db497633b8b5ddef3c7669bf6575edf3afad190f
Simen Lyse <simenlyse@gmail.com>
2019-05-21 11:10:48 +0200

    Glemt hva jeg har gjort

--- src/eksamen2019/Main.java
+++ src/eksamen2019/Main.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
+
 
 public class Main extends JFrame implements ActionListener {
 	private Kontroll kontroll = Kontroll.getInstance();
